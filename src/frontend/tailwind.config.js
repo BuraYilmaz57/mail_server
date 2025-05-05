2df119fbbfe209d8f88eacee814f60c0a935cdc6
Burak Yilmaz <burak.yilmaz@sabanciuniv.edu>
2025-05-05 23:51:21 +0300

    Fixed Frontend

--- src/frontend/tailwind.config.js
+++ src/frontend/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}", // this is needed to scan your React files
   ],
   theme: {
     extend: {},
